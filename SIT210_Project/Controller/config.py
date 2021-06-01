e0c3fe0ab6f89133254f2396406c0d4985dc28fd
allanfarrell <afarrell264@hotmail.com>
2021-06-01 13:22:46 +0700

    Update config.py

--- SIT210_Project/Controller/config.py
+++ SIT210_Project/Controller/config.py
@@ -8,17 +8,14 @@ TOPIC="RFID"
 TOPIC2="CONTROL"
 
 ## Global variables shared between files
-#
+# MQTT client
 ourClient = ''
-#
+# ScrolledText box widget
 ourConsole = ''
-#
-#resultScrolledText = mtk.scrolledtext.ScrolledText()
-#
+# Toggle device On/Off button
 submitBtn = ''
-
-# Recording the device state (RFID/All)
+# Display CONTROL topic when in debug mode
 debugMode = True
 
-#
+# TO BE REMOVED
 topicsDISPLAY = 'TO BE REMOVED'
